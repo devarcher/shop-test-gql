f2e51195f723f94514e4844f6895fd2d2b4fdde8
devarcher <archershot@hotmail.com>
2020-04-05 23:46:15 -0500

    successful auth & updated scope for orders

---
+++ pages/_app.js
@@ -0,0 +1,30 @@
+import '../styles/index.css'
+import App from "next/app";
+import Head from "next/head";
+import { Provider } from '@shopify/app-bridge-react';
+import Cookies from 'js-cookie';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+
+const client = new ApolloClient({
+  fetchOptions: {
+    credentials: 'include'
+  },
+});
+
+class MyApp extends App {
+  render() {
+    const { Component, pageProps } = this.props;
+    return (
+      <React.Fragment>
+        <Head>
+          <title>Shopify Tailwind Test</title>
+          <meta charSet="utf-8" />
+        </Head>
+        <Component {...pageProps} />
+      </React.Fragment>
+    );
+  }
+}
+
+export default MyApp;
