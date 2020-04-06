5cd37ef1aff07f037ef54918b5f0d758751c690d
devarcher <archershot@hotmail.com>
2020-04-06 17:43:42 -0500

    orders query - added customer info and custom attributes to search. filtered query by fulfillment status

--- pages/orders.js
+++ pages/orders.js
@@ -2,18 +2,32 @@ import gql from 'graphql-tag';
 import { Query } from 'react-apollo';
 
 const GET_ORDERS = gql`
-  {
+{
  	shop {
     id
     name
   }
-  orders(first:10) {
+  orders(first: 10, query: "fulfillment_status:unshipped") {
     edges {
       node {
         id
         name
         displayFulfillmentStatus
         note
+        createdAt
+        customer {
+          id
+          firstName
+          lastName
+          email
+          phone
+          ordersCount
+          totalSpent
+        }
+        customAttributes {
+          key
+          value
+        }
       }
     }
   }
@@ -23,18 +37,25 @@ const GET_ORDERS = gql`
 const Orders = () => {
   return (
     <>
-    <h1>Orders</h1>
+    <h1>Unfulfilled Orders</h1>
     <Query query={GET_ORDERS}>
       {({ data, loading, error }) => {
           if (loading) return <div>Loading…</div>;
           if (error) return <div>{error.message}</div>;
           console.log(data.orders.edges);
+          
+          const pickupMethod = data.orders.edges[7].customAttributes
 
-          return data.orders.edges.map((edge, index) => (
-          <div key={index} className="border p-2">
-            <h3>Order Name: {edge.node.name}</h3>
+          console.log(pickupMethod)
+
+          return data.orders.edges.map((edge) => (
+          <div key={edge.node.name} className="border p-2">
+            <h2>Order Name: {edge.node.name}</h2>
             <h4>Status: {edge.node.displayFulfillmentStatus}</h4>
-            <h4>Customer message: {edge.node.note}</h4>
+            <h3>Customer Name: {edge.node.customer.firstName} {edge.node.customer.lastName}</h3>
+            <h3>Customer Email: {edge.node.customer.email}</h3>
+            {edge.node.note && <h4>Customer message: {edge.node.note}</h4>}
+            <h4>Time Created: {edge.node.createdAt}</h4>
           </div>
           ))
 
